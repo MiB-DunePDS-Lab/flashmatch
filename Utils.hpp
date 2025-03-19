ec01f1afc395be710a3b3f6e49cd69aa72c8eb17
fgalizzi <fedegal99@gmail.com>
2025-03-19 11:39:24 -0500

    Move methods in Utils.hpp

---
+++ Utils.hpp
@@ -0,0 +1,43 @@
+// From the hgridX histograms created by the PhotonVisibilityExpoort_module.cc, it returns
+// a vector where the z-th + y-th * hgrid[2]->GetNbinsX() + x-th * hgrid[2]->GetNbinsX()*hgrid[1]->GetNbinsX()
+// entry value is the number of the photoVisMap entry corresponding to the x,y,z coordinates.
+std::vector<int> GetCryoToTPCMap(TH1D* hgrid[3], double tpc_min[3], double tpc_max[3]){
+  size_t n_total_entries = hgrid[0]->GetNbinsX() * hgrid[1]->GetNbinsX() * hgrid[2]->GetNbinsX();
+  std::vector<int> total_to_tpc(n_total_entries, -1);
+  int tpc_entry = 0;
+  for (int ix=0; ix<hgrid[0]->GetNbinsX(); ix++){
+    for (int iy=0; iy<hgrid[1]->GetNbinsX(); iy++){
+      for (int iz=0; iz<hgrid[2]->GetNbinsX(); iz++){
+        double x = hgrid[0]->GetBinCenter(ix);
+        double y = hgrid[1]->GetBinCenter(iy);
+        double z = hgrid[2]->GetBinCenter(iz);
+        
+        if (x > tpc_min[0] && x < tpc_max[0] && 
+            y > tpc_min[1] && y < tpc_max[1] && 
+            z > tpc_min[2] && z < tpc_max[2]) {
+          total_to_tpc[iz + iy*hgrid[2]->GetNbinsX() + ix*hgrid[2]->GetNbinsX()*hgrid[1]->GetNbinsX()] = tpc_entry;
+          tpc_entry++;
+        }
+        
+      }
+    }
+  }
+  return total_to_tpc;
+}
+
+bool isInTPC(double vertex_coor[3], double tpc_min[3], double tpc_max[3]){
+  return (vertex_coor[0] > tpc_min[0] && vertex_coor[0] < tpc_max[0] &&
+          vertex_coor[1] > tpc_min[1] && vertex_coor[1] < tpc_max[1] &&
+          vertex_coor[2] > tpc_min[2] && vertex_coor[2] < tpc_max[2]);
+}
+
+int GetTPCIndex(double vertex_coor[3], TH1D* hgrid[3], std::vector<int>& total_to_tpc){
+  int bin_x = hgrid[0]->FindBin(vertex_coor[0]);
+  int bin_y = hgrid[1]->FindBin(vertex_coor[1]);
+  int bin_z = hgrid[2]->FindBin(vertex_coor[2]);
+
+  int tpc_index = total_to_tpc[bin_z + bin_y*hgrid[2]->GetNbinsX()
+                               + bin_x*hgrid[2]->GetNbinsX()*hgrid[1]->GetNbinsX()];
+
+  return tpc_index; // Inside TPC
+}
