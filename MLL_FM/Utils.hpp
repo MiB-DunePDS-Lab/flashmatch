4bcf49f8517ed1420fda15d49de23091620058a3
fgalizzi <fedegal99@gmail.com>
2026-03-03 15:49:33 +0100

    Remove unused functionss

--- MLL_FM/Utils.hpp
+++ MLL_FM/Utils.hpp
@@ -115,26 +115,6 @@ inline float give_me_Ereco(float calib_c, float calib_slope, float corr_lambda,
   return E_reco;
 }
 
-inline TH2D* get_normalized_bidistribution(TH2D* h2_bidistr){
-  TH2D* h2_bidistr_norm = new TH2D(Form("%s_norm", h2_bidistr->GetName()),
-                                   Form("%s_norm;%s;%s", h2_bidistr->GetName(), h2_bidistr->GetXaxis()->GetTitle(), h2_bidistr->GetYaxis()->GetTitle()),
-                                   h2_bidistr->GetNbinsX(), h2_bidistr->GetXaxis()->GetXmin(), h2_bidistr->GetXaxis()->GetXmax(),
-                                   h2_bidistr->GetNbinsY(), h2_bidistr->GetYaxis()->GetXmin(), h2_bidistr->GetYaxis()->GetXmax());
-  for(int idx_y=1; idx_y<=h2_bidistr->GetNbinsY(); idx_y++){
-    TH1D* h1_proj = h2_bidistr->ProjectionX("h1_proj", idx_y, idx_y);
-    double h1_proj_integral = h1_proj->Integral();
-    
-    if(h1_proj_integral > 0.){
-      h1_proj->Scale(1./(h1_proj_integral*h1_proj->GetBinWidth(1)));
-
-      for(int idx_x=1; idx_x<=h2_bidistr->GetNbinsX(); idx_x++){
-        h2_bidistr_norm->SetBinContent(idx_x, idx_y, h1_proj->GetBinContent(idx_x));
-      } 
-    }
-  }
-  return h2_bidistr_norm;
-}
-
 
 // --- HANDLE VISIBILITIES ----------------------------------------------------
 
