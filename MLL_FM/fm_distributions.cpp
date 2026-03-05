fbc3be3b9e7f99e6848813393aec3177930b6cff
fgalizzi <fedegal99@gmail.com>
2026-03-05 11:47:22 +0100

    Cleaning

--- MLL_FM/fm_distributions.cpp
+++ MLL_FM/fm_distributions.cpp
@@ -12,48 +12,6 @@
 #include "Utils.hpp"
 
 
-bool get_next_entry(TTree* tree, int& entry, int& entries){
-  if (++entry>entries) return false;
-  tree->GetEntry(entry);
-  return true;
-}
-
-
-template <typename T>
-T weighted_average(const std::vector<T>& values, const std::vector<T>& weights) {
-
-  T weighted_sum = 0.0;
-  T total_weight = 0.0;
-
-  for (size_t i = 0; i < values.size(); ++i) {
-    weighted_sum += values[i] * weights[i];
-    total_weight += weights[i];
-  }
-
-  return weighted_sum / total_weight;
-}
-
-TH2D* get_normalized_bidistribution(TH2D* h2_bidistr){
-  TH2D* h2_bidistr_norm = new TH2D(Form("%s_norm", h2_bidistr->GetName()),
-                                   Form("%s_norm;%s;%s", h2_bidistr->GetName(), h2_bidistr->GetXaxis()->GetTitle(), h2_bidistr->GetYaxis()->GetTitle()),
-                                   h2_bidistr->GetNbinsX(), h2_bidistr->GetXaxis()->GetXmin(), h2_bidistr->GetXaxis()->GetXmax(),
-                                   h2_bidistr->GetNbinsY(), h2_bidistr->GetYaxis()->GetXmin(), h2_bidistr->GetYaxis()->GetXmax());
-  for(int idx_y=1; idx_y<=h2_bidistr->GetNbinsY(); idx_y++){
-    TH1D* h1_proj = h2_bidistr->ProjectionX("h1_proj", idx_y, idx_y);
-    double h1_proj_integral = h1_proj->Integral();
-    
-    if(h1_proj_integral > 0.){
-      h1_proj->Scale(1./(h1_proj_integral*h1_proj->GetBinWidth(1)));
-
-      for(int idx_x=1; idx_x<=h2_bidistr->GetNbinsX(); idx_x++){
-        h2_bidistr_norm->SetBinContent(idx_x, idx_y, h1_proj->GetBinContent(idx_x));
-      } 
-    }
-  }
-  return h2_bidistr_norm;
-}
-
-
 void fm_distributions(){
   // --- CONFIGS ---------------------------------------------------------------
   MLLcconfigs f = load_ana_config("./config.json");
@@ -123,6 +81,7 @@ void fm_distributions(){
   float x_true, y_true, z_true, e_true;
   std::vector<float>* reco_pes          = nullptr;
   std::vector<float>* exp_phs           = nullptr;
+  std::vector<int>* opdets           = nullptr;
 
   tpc_pds_tree->Branch("ifile",     &ifile);
   tpc_pds_tree->Branch("iev",       &iev);
@@ -136,10 +95,14 @@ void fm_distributions(){
   tpc_pds_tree->Branch("time_pds",  &time_pds);
   tpc_pds_tree->Branch("reco_pes",  &reco_pes);
   tpc_pds_tree->Branch("exp_phs",   &exp_phs);
+  tpc_pds_tree->Branch("opdets",    &opdets);
   tpc_pds_tree->Branch("x_true",    &x_true);
   tpc_pds_tree->Branch("y_true",    &y_true);
   tpc_pds_tree->Branch("z_true",    &z_true);
   tpc_pds_tree->Branch("e_true",    &e_true);
+  for(size_t i=0; i<n_opdet; i++){
+    opdets->push_back(i);
+  }
   
   TH1D* h_exp = new TH1D("h_exp",
                          Form("%s;%s;%s","h_exp","Expected #Pe","Counts"),
@@ -167,14 +130,17 @@ void fm_distributions(){
     idx_file++;
     if(!std::filesystem::exists(ana_file_name)) continue;
     ifile = idx_file; nfile_analyzed++;
-    /* if (idx_file % 10 == 0) */ std::cout << nfile_analyzed <<"--"<< ana_file_name << std::endl;
+    if (idx_file % 10 == 0) std::cout <<nfile_analyzed<<"--"<< ana_file_name << "\r" << std::flush;
 
     TFile* ana_file = TFile::Open(ana_file_name.c_str(), "READ");
     TTree* solarnu_tree = static_cast<TTree*>(ana_file->Get("solarnuana/SolarNuAnaTree"));
+    std::vector<size_t> MaxChargeIndxs = take_max_charge_indices(solarnu_tree, "Event", "Charge");
+
     int sn_iev = 0; int sn_flag = 0;
     float TrueX = 0.; float TrueY = 0.; float TrueZ = 0.;
     float TrueE = 0.;
     float MatchedOpFlashRecoX = 0.;
+    float MatchedOpFlashPurity = 0.;
     // float MatchedOpFlashRecoY = 0.; float MatchedOpFlashRecoZ = 0.;
     float MatchedOpFlashTime = 0.;
     bool MatchedOpFlashCorrectly = false;
@@ -196,6 +162,7 @@ void fm_distributions(){
     // solarnu_tree->SetBranchAddress("MatchedOpFlashRecoY",      &MatchedOpFlashRecoY);
     // solarnu_tree->SetBranchAddress("MatchedOpFlashRecoZ",      &MatchedOpFlashRecoZ);
     solarnu_tree->SetBranchAddress("MatchedOpFlashTime",       &MatchedOpFlashTime);
+    solarnu_tree->SetBranchAddress("MatchedOpFlashPur",       &MatchedOpFlashPurity);
     solarnu_tree->SetBranchAddress("MatchedOpFlashCorrectly",  &MatchedOpFlashCorrectly);
     solarnu_tree->SetBranchAddress("MatchedOpFlashPEperOpDet", &MatchedOpFlashPEperOpDet);
     solarnu_tree->SetBranchAddress("Charge",                   &Charge);
@@ -204,11 +171,15 @@ void fm_distributions(){
     int sn_entries = solarnu_tree->GetEntries();
     for (int sn_entry = 0; sn_entry < sn_entries; sn_entry++) {
       solarnu_tree->GetEntry(sn_entry);
+    // for (auto& idx_entry : MaxChargeIndxs){
+    //   solarnu_tree->GetEntry(idx_entry);
       if (!MatchedOpFlashCorrectly) continue;
       iev = sn_iev;
 
       x_reco = MatchedOpFlashRecoX; y_reco = RecoY; z_reco = RecoZ;
       x_true = TrueX; y_true = TrueY; z_true = TrueZ; e_true = TrueE;
+      // if (MatchedOpFlashPurity<0.9) continue;
+   
 
       vertex_coor[0] = TrueX; vertex_coor[1] = TrueY; vertex_coor[2] = TrueZ;
       if(!isInFiducialVolume(vertex_coor, vol_min, vol_max, x_cut)) continue;
@@ -249,6 +220,9 @@ void fm_distributions(){
           h_exp->Fill(exp_ph);
           h_expreco->Fill(exp_ph);
           h2_exp_reco->Fill(reco_pe, exp_ph);
+          // if (exp_ph < 50 && reco_pe > 200) {
+          //   std::cout << TrueX << " " << TrueY << " " << TrueZ <<" "<< TrueE <<" "<< e_reco <<" "<< e_reco/TrueE << std::endl;
+          // }
         }
         else {
           h_exp->Fill(exp_ph);
@@ -266,8 +240,6 @@ void fm_distributions(){
   he_hit_prob->SetTitle("Hit Probability;Expected #Pe;Reconstruction Probability");
   he_hit_prob->SetName("he_hit_prob");
 
-  TH2D* h2_exp_reco_norm = get_normalized_bidistribution(h2_exp_reco);
-
   TTree* pds_tree = new TTree("pds_tree", "pds_tree");
   pds_tree->Branch("n_opdet", &n_opdet);
   pds_tree->Branch("LY_times_PDE", &LY_times_PDE);
@@ -281,7 +253,6 @@ void fm_distributions(){
   h_expreco->Write();
   he_hit_prob->Write();
   h2_exp_reco->Write();
-  h2_exp_reco_norm->Write();
   out_file->Close();
 
   return;
